b3ebd71060fc8eff9e6d29d7fc0d8dbd0d70e885
JoaoFreire1 <joao.freire.art@gmail.com>
2025-03-21 11:28:42 +0000

    Exercises

---
+++ SoGoodLib/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SoGoodLib
+{
+    public class Program
+    {
+        public static string SoGoodMethod()
+        {
+            return "I came from another project!";
+        }
+
+    }
+}
