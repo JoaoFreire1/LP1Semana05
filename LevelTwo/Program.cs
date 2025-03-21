b17cddc8da34f257280f829bd024099418977675
JoaoFreire1 <joao.freire.art@gmail.com>
2025-03-21 11:23:18 +0000

    Show image

--- LevelTwo/Program.cs
+++ LevelTwo/Program.cs
@@ -6,7 +6,18 @@ namespace LevelTwo
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            Console.WriteLine(SoGoodClass.SoGoodMethod());
+            if (args.Length == 2)
+                ShowImg(args[0], int.Parse(args[1]));
+            else
+                ShowImg("tux.jpg", 24);
+        }
+
+        private static void ShowImg(string imgName, int size)
+        {
+            CanvasImage img = new CanvasImage(imgName);
+            img.MaxWidth(size);
+            AnsiConsole.Write(img);
         }
     }
 }
